76bf2c894e75a440ce303a2bbd5b1a0175c4cb50
Moshua53 <moisesjoshua53@gmail.com>
2025-08-01 07:03:25 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
+def calcular_ahorro_mesual(meta:int, meses:int, abono:int)->float:
+    return (meta/meses)+abono
 
+def prueba():
+    #Entradas
+    meta= 1_200_000
+    meses= 12
+    abonos_extra= 0
+
+    #Probar salidas
+    ahorro_mensual= calcular_ahorro_mesual(meta, meses, abonos_extra)
+
+    #Salidas
+    ahorro_mensual_esperado = 100_000
+
+    if ahorro_mensual_esperado == ahorro_mensual:
+        print("Prueba pasada")
+    else:
+        print("Prueba fallida")
+
+prueba()

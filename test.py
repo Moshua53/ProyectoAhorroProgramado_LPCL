8d61db3bcc4a677d94d5d68af71aac74e0ce8448
Moshua53 <moisesjoshua53@gmail.com>
2025-08-01 07:14:54 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 def calcular_ahorro_mesual(meta:int, meses:int, abono:int)->float:
+    """Permite calcular el ahorro mesual que se debe realizar para llegar a la meta del ahorro programado"""
     return (meta/meses)+abono
 
 def prueba():

10a1ebcade77c0d20ea8cd736e59e1539c5ab6b1
Moshua53 <moisesjoshua53@gmail.com>
2025-08-06 07:10:14 -0500

    Add files via upload

---
+++ Modulos.py
@@ -0,0 +1,3 @@
+def calcular_ahorro_mesual(meta: int, meses: int, abono: int) -> float:
+    """ Permite calcular el ahorro mesual que se debe realizar para llegar a la meta del ahorro programado """
+    return (meta / meses) + abono

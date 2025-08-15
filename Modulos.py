9e47e93e16b9efd9668598bba9c2e1f14ad31e1e
Moshua53 <moisesjoshua53@gmail.com>
2025-08-14 21:36:27 -0500

    Update Modulos.py

--- Modulos.py
+++ Modulos.py
@@ -1,3 +1,13 @@
 def calcular_ahorro_mesual(meta: int, meses: int, abono: int) -> float:
-    """ Permite calcular el ahorro mesual que se debe realizar para llegar a la meta del ahorro programado """
+    """ Permite calcular el ahorro mensual que se debe realizar para llegar a la meta del ahorro programado """
+
+    if meta < 0:
+        raise ValueError("La meta no puede ser negativa")
+
+    if meses <= 0:
+        raise ValueError("El número de meses debe ser mayor que cero")
+
+    if (meta / meses) + abono > meta:
+        raise ValueError("La meta ya fue superada con los abonos, ahorro innecesario")
+
     return (meta / meses) + abono

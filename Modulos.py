9190825eb8c39ae175e516758ce7a0476d618524
Moshua53 <moisesjoshua53@gmail.com>
2025-08-15 07:12:16 -0500

    Update Modulos.py

--- Modulos.py
+++ Modulos.py
@@ -1,11 +1,19 @@
-class ErrorValorAhorro( Exception ):
+class ErrorValorMeta( Exception ):
     """ Valor de la meta debe ser mayor a cero """
 
     def __init__ (self, *args):
-        super().__init__("ERROR: El valor de la meta debe ser mayor que cero
+        super().__init__("ERROR: El valor de la meta debe ser mayor que cero")
+                         
+class ErrorValorMeses( Exception ):
+    """Valor de los meses debe ser mayor que cero """
+
+    def __init__ (self, *args):
+        super().__init__("ERROR: el valor de los meses debe ser mayor que cero")
 
 class ErrorAbonoExcesivo( Exception ):
-    pass
+    """La meta ya fue superada con los abonos, ahorro innecesario"""
+    def __init__ (self, *args):
+        super().__init__("ERROR: El valor de la meta debe ser mayor que cero")
 
 def calcular_ahorro_mesual(meta: int, meses: int, abono: int) -> float:
     """ Permite calcular el ahorro mensual que se debe realizar para llegar a la meta del ahorro programado """
@@ -20,5 +28,6 @@ def calcular_ahorro_mesual(meta: int, meses: int, abono: int) -> float:
         raise ValueError("La meta ya fue superada con los abonos, ahorro innecesario")
 
     return (meta / meses) + abono
+
 
 

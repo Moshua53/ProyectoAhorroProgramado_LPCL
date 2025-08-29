d1611f8a8bb8a1381ab095a041b64023030c7884
Mateo Molina González <89753117+molxeuz@users.noreply.github.com>
2025-08-29 07:32:47 -0500

    Update interfaz.py

--- src/view/interfaz.py
+++ src/view/interfaz.py
@@ -11,7 +11,7 @@ try:
   abono_extra = float(input("Ingrese el abono extra: "))
 
   # Realizar procesos
-  ahorro_mensual = Modulos.calcular_ahorro_mesual(meta, meses, abono_extra)
+  ahorro_mensual = Modulos.calcular_ahorro_mensual(meta, meses, abono_extra)
 
   # Mostrar datos de salida
   print(f"El valor del ahorro mensual es: {ahorro_mensual}")

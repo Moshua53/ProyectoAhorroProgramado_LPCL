654054df2c5ef843c7b03163bffec815146af9d0
Moshua53 <moisesjoshua53@gmail.com>
2025-10-18 23:06:07 -0500

    Set 'cedula' as primary key in 'usuarios' table

--- sql/crear_usuarios.sql
+++ sql/crear_usuarios.sql
@@ -1,8 +1,7 @@
-
 -- Crea la tabla de usuarios
 create table usuarios (
-  cedula varchar( 20 )  NOT NULL,
+  cedula varchar( 20 )  NOT NULL PRIMARY KEY,
   nombre text not null,
   apellido text not null,
-  correo text,
+  correo text
 ); 

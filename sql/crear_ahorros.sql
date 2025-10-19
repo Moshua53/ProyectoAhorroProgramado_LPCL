9de64a70d697c906d6e048e25c6ef711c32b0eac
Moshua53 <moisesjoshua53@gmail.com>
2025-10-18 23:05:28 -0500

    Add foreign key reference to usuarios in ahorros table

--- sql/crear_ahorros.sql
+++ sql/crear_ahorros.sql
@@ -1,8 +1,6 @@
-
--- Crea la tabla de ahorros
 CREATE TABLE IF NOT EXISTS ahorros (
   id_ahorro SERIAL PRIMARY KEY,
-  cedula VARCHAR(20) NOT NULL,
+  cedula VARCHAR(20) NOT NULL REFERENCES usuarios(cedula),
   meta NUMERIC(14,2) NOT NULL,
   meses INT NOT NULL CHECK (meses > 0),
   abonos_extras NUMERIC(14,2) DEFAULT 0,

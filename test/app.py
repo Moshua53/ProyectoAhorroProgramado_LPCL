f513f7ad2f33bb7e8021c4d6ee86a5f3b1ba79c2
Mateo Molina González <89753117+molxeuz@users.noreply.github.com>
2025-10-29 07:40:43 -0500

    Update app.py

--- test/app.py
+++ test/app.py
@@ -3,9 +3,20 @@ from flask import Flask
 
 from flask import render_template, request
 
+from model.ahorros import Ahorro
+
 app = Flask(__name__)
 
 @app.route('/')
+def calcular_ahorro():
+    meta = float( request.args["compra"] )
+    meses = int( request.args["meses"] )
+    abono = float( request.args["abono"] )
+    intereses = float( request.args["intereses"] )
+
+    result = Ahorro.""(meta, meses, abono, intereses)
+    return f"Aqui se calcula el ahorro, la meta fue de {meta} a {meses} meses, con un abono de {abono}$ con una tasa de interes del {intereses}. para un ahorro mensual de {result}"
+
 def hello():
     return render_template("ahorro.html")
 
